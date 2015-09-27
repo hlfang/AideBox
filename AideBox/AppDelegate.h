b480cb7d65a6cb14a5f0b18b6534e487bc2ea6c5
hlfang <fanghailong_168@qq.com>
2015-09-27 15:55:52 +0800

    Initial Commit

---
+++ AideBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AideBox
+//
+//  Created by 方海龙 on 15/9/27.
+//  Copyright (c) 2015年 方海龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AideBox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AideBox
+//
+//  Created by 方海龙 on 15/9/27.
+//  Copyright (c) 2015年 方海龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
